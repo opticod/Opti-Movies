01d9ecfefc560d2e18c1d6520d697a3d7cd0fb84
Anupam Das <anupam.das.bwn@gmail.com>
2015-12-18 02:11:30 +0530

    REFACTOR database slightly changed

--- app/src/main/java/work/technie/popularmovies/data/MovieContract.java
+++ app/src/main/java/work/technie/popularmovies/data/MovieContract.java
@@ -11,15 +11,11 @@ public class MovieContract{
 
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
-    public static final String PATH_SORT_BY_POP = "popular";
-    public static final String PATH_SORT_BY_REV = "revenue";
-    public static final String PATH_SORT_BY_VOTE = "rated";
-    public static final String PATH_BOOKMARKS = "bookmarks";
+    public static final String PATH_MOVIES = "movies";
 
+    public static final class Movies implements BaseColumns{
 
-    public static final class PopularMovies implements BaseColumns{
-
-        public static final String TABLE_NAME = "popular";
+        public static final String TABLE_NAME = "movies";
 
         public static final String PAGE = "page";
         public static final String  POSTER_PATH= "poster_path";
@@ -34,15 +30,15 @@ public class MovieContract{
         public static final String  POPULARITY= "popularity";
         public static final String  VOTE_COUNT= "vote_count";
         public static final String  VOTE_AVERAGE= "vote_average";
-
+        public static final String FAVOURED="favoured";
 
         public static final Uri CONTENT_URI =
-                    BASE_CONTENT_URI.buildUpon().appendPath(PATH_SORT_BY_POP).build();
+                    BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
 
         public static final String CONTENT_TYPE =
-                    ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SORT_BY_POP;
+                    ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIES;
         public static final String CONTENT_ITEM_TYPE =
-                    ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SORT_BY_POP;
+                    ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIES;
 
         public static Uri buildPopularUri(long id) {
                 return ContentUris.withAppendedId(CONTENT_URI, id);
@@ -53,101 +49,6 @@ public class MovieContract{
         }
 
 
-    }
-    public static final class RevenueMovies implements BaseColumns{
-
-        public static final String TABLE_NAME = "revenue";
-
-        public static final String PAGE = "page";
-        public static final String  POSTER_PATH= "poster_path";
-        public static final String  ADULT= "adult";
-        public static final String  OVERVIEW= "overview";
-        public static final String  RELEASE_DATE= "release_date";
-        public static final String  MOVIE_ID= "id";
-        public static final String  ORIGINAL_TITLE= "original_title";
-        public static final String  ORIGINAL_LANGUAGE= "original_language";
-        public static final String  TITLE= "title";
-        public static final String  BACKDROP_PATH= "backdrop_path";
-        public static final String  POPULARITY= "popularity";
-        public static final String  VOTE_COUNT= "vote_count";
-        public static final String  VOTE_AVERAGE= "vote_average";
-
-
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_SORT_BY_REV).build();
-
-        public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SORT_BY_REV;
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SORT_BY_REV;
-
-        public static Uri buildRevenueUri(long id) {
-            return ContentUris.withAppendedId(CONTENT_URI, id);
-
-        }
-    }
-    public static final class RatedMovies implements BaseColumns{
-
-        public static final String TABLE_NAME = "rated";
-
-        public static final String PAGE = "page";
-        public static final String  POSTER_PATH= "poster_path";
-        public static final String  ADULT= "adult";
-        public static final String  OVERVIEW= "overview";
-        public static final String  RELEASE_DATE= "release_date";
-        public static final String  MOVIE_ID= "id";
-        public static final String  ORIGINAL_TITLE= "original_title";
-        public static final String  ORIGINAL_LANGUAGE= "original_language";
-        public static final String  TITLE= "title";
-        public static final String  BACKDROP_PATH= "backdrop_path";
-        public static final String  POPULARITY= "popularity";
-        public static final String  VOTE_COUNT= "vote_count";
-        public static final String  VOTE_AVERAGE= "vote_average";
-
-
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_SORT_BY_VOTE).build();
-
-        public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SORT_BY_VOTE;
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SORT_BY_VOTE;
-
-        public static Uri buildRatedUri(long id) {
-            return ContentUris.withAppendedId(CONTENT_URI, id);
-
-        }
-    }public static final class Bookmarks implements BaseColumns{
-
-        public static final String TABLE_NAME = "bookmarks";
-
-         public static final String PAGE = "page";
-        public static final String  POSTER_PATH= "poster_path";
-        public static final String  ADULT= "adult";
-        public static final String  OVERVIEW= "overview";
-        public static final String  RELEASE_DATE= "release_date";
-        public static final String  MOVIE_ID= "id";
-        public static final String  ORIGINAL_TITLE= "original_title";
-        public static final String  ORIGINAL_LANGUAGE= "original_language";
-        public static final String  TITLE= "title";
-        public static final String  BACKDROP_PATH= "backdrop_path";
-        public static final String  POPULARITY= "popularity";
-        public static final String  VOTE_COUNT= "vote_count";
-        public static final String  VOTE_AVERAGE= "vote_average";
-
-
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_BOOKMARKS).build();
-
-        public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKMARKS;
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKMARKS;
-
-        public static Uri buildBookmarksUri(long id) {
-            return ContentUris.withAppendedId(CONTENT_URI, id);
-
-        }
     }
 
 }
--- app/src/main/java/work/technie/popularmovies/data/MovieProvider.java
+++ app/src/main/java/work/technie/popularmovies/data/MovieProvider.java
@@ -11,39 +11,19 @@ import android.net.Uri;
 
 public class MovieProvider extends ContentProvider {
 
-    // The URI Matcher used by this content provider.
     private static final UriMatcher sUriMatcher = buildUriMatcher();
     private MovieDBHelper mOpenHelper;
 
-    static final int POPULAR = 100;
-    static final int POPULAR_WITH_ID = 101;
-    static final int REVENUE = 200;
-    static final int REVENUE_WITH_ID = 201;
-    static final int RATED = 300;
-    static final int RATED_WITH_ID = 301;
-    static final int BOOKMARKS = 400;
-    static final int BOOKMARKS_WITH_ID = 401;
+    static final int MOVIE = 100;
+    static final int MOVIE_WITH_ID = 101;
 
     static UriMatcher buildUriMatcher() {
 
-        // All paths added to the UriMatcher have a corresponding code to return when a match is
-        // found.  The code passed into the constructor represents the code to return for the root
-        // URI.  It's common to use NO_MATCH as the code for this case.
         final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
         final String authority = MovieContract.CONTENT_AUTHORITY;
 
-        // For each type of URI you want to add, create a corresponding code.
-        matcher.addURI(authority, MovieContract.PATH_SORT_BY_POP, POPULAR);
-        matcher.addURI(authority, MovieContract.PATH_SORT_BY_POP + "/*", POPULAR_WITH_ID);
-
-        matcher.addURI(authority, MovieContract.PATH_SORT_BY_REV, REVENUE);
-        matcher.addURI(authority, MovieContract.PATH_SORT_BY_POP + "/*", REVENUE_WITH_ID);
-
-        matcher.addURI(authority, MovieContract.PATH_SORT_BY_VOTE, RATED);
-        matcher.addURI(authority, MovieContract.PATH_SORT_BY_POP + "/*", RATED_WITH_ID);
-
-        matcher.addURI(authority, MovieContract.PATH_BOOKMARKS, BOOKMARKS);
-        matcher.addURI(authority, MovieContract.PATH_SORT_BY_POP + "/*", BOOKMARKS_WITH_ID);
+        matcher.addURI(authority, MovieContract.PATH_MOVIES, MOVIE);
+        matcher.addURI(authority, MovieContract.PATH_MOVIES + "/*", MOVIE_WITH_ID);
 
         return matcher;
     }
@@ -61,25 +41,11 @@ public class MovieProvider extends ContentProvider {
         final int match = sUriMatcher.match(uri);
 
         switch (match) {
-            case POPULAR_WITH_ID:
-                return MovieContract.PopularMovies.CONTENT_ITEM_TYPE;
-            case POPULAR:
-                return MovieContract.PopularMovies.CONTENT_TYPE;
 
-            case RATED_WITH_ID:
-                return MovieContract.RatedMovies.CONTENT_ITEM_TYPE;
-            case RATED:
-                return MovieContract.RatedMovies.CONTENT_TYPE;
-
-            case REVENUE_WITH_ID:
-                return MovieContract.RevenueMovies.CONTENT_ITEM_TYPE;
-            case REVENUE:
-                return MovieContract.RevenueMovies.CONTENT_TYPE;
-
-            case BOOKMARKS_WITH_ID:
-                return MovieContract.Bookmarks.CONTENT_ITEM_TYPE;
-            case BOOKMARKS :
-                return MovieContract.Bookmarks.CONTENT_TYPE;
+            case MOVIE :
+            return MovieContract.Movies.CONTENT_TYPE;
+            case MOVIE_WITH_ID:
+                return MovieContract.Movies.CONTENT_ITEM_TYPE;
 
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
@@ -93,9 +59,9 @@ public class MovieProvider extends ContentProvider {
         // and query the database accordingly.
         Cursor retCursor;
         switch (sUriMatcher.match(uri)) {
-            case POPULAR: {
+            case MOVIE: {
                 retCursor = mOpenHelper.getReadableDatabase().query(
-                        MovieContract.PopularMovies.TABLE_NAME,
+                        MovieContract.Movies.TABLE_NAME,
                         projection,
                         selection,
                         selectionArgs,
@@ -105,81 +71,11 @@ public class MovieProvider extends ContentProvider {
                 );
                 break;
             }
-            case POPULAR_WITH_ID:{
+            case MOVIE_WITH_ID:{
                 retCursor = mOpenHelper.getReadableDatabase().query(
-                        MovieContract.PopularMovies.TABLE_NAME,
+                        MovieContract.Movies.TABLE_NAME,
                         projection,
-                        MovieContract.PopularMovies.MOVIE_ID+" = ?",
-                        new String[] {String.valueOf(ContentUris.parseId(uri))},
-                        null,
-                        null,
-                        sortOrder);
-                break;
-            }
-            case REVENUE: {
-                retCursor = mOpenHelper.getReadableDatabase().query(
-                        MovieContract.RevenueMovies.TABLE_NAME,
-                        projection,
-                        selection,
-                        selectionArgs,
-                        null,
-                        null,
-                        sortOrder
-                );
-                break;
-            }
-            case REVENUE_WITH_ID:{
-                retCursor = mOpenHelper.getReadableDatabase().query(
-                        MovieContract.RevenueMovies.TABLE_NAME,
-                        projection,
-                        MovieContract.RevenueMovies.MOVIE_ID+" = ?",
-                        new String[] {String.valueOf(ContentUris.parseId(uri))},
-                        null,
-                        null,
-                        sortOrder);
-                break;
-            }
-            case RATED: {
-                retCursor = mOpenHelper.getReadableDatabase().query(
-                        MovieContract.RatedMovies.TABLE_NAME,
-                        projection,
-                        selection,
-                        selectionArgs,
-                        null,
-                        null,
-                        sortOrder
-                );
-                break;
-            }
-            case RATED_WITH_ID:{
-                retCursor = mOpenHelper.getReadableDatabase().query(
-                        MovieContract.RatedMovies.TABLE_NAME,
-                        projection,
-                        MovieContract.RatedMovies.MOVIE_ID+" = ?",
-                        new String[] {String.valueOf(ContentUris.parseId(uri))},
-                        null,
-                        null,
-                        sortOrder);
-                break;
-            }
-            case BOOKMARKS: {
-                retCursor = mOpenHelper.getReadableDatabase().query(
-                        MovieContract.Bookmarks.TABLE_NAME,
-                        projection,
-                        selection,
-                        selectionArgs,
-                        null,
-                        null,
-                        sortOrder
-                );
-                break;
-            }
-
-            case BOOKMARKS_WITH_ID:{
-                retCursor = mOpenHelper.getReadableDatabase().query(
-                        MovieContract.Bookmarks.TABLE_NAME,
-                        projection,
-                        MovieContract.Bookmarks.MOVIE_ID+" = ?",
+                        MovieContract.Movies.MOVIE_ID+" = ?",
                         new String[] {String.valueOf(ContentUris.parseId(uri))},
                         null,
                         null,
@@ -201,34 +97,10 @@ public class MovieProvider extends ContentProvider {
         Uri returnUri;
 
         switch (match) {
-            case POPULAR: {
-                long _id = db.insert(MovieContract.PopularMovies.TABLE_NAME, null, values);
+            case MOVIE: {
+                long _id = db.insert(MovieContract.Movies.TABLE_NAME, null, values);
                 if ( _id > 0 )
-                    returnUri = MovieContract.PopularMovies.buildPopularUri(_id);
-                else
-                    throw new android.database.SQLException("Failed to insert row into " + uri);
-                break;
-            }
-            case RATED: {
-                long _id = db.insert(MovieContract.RatedMovies.TABLE_NAME, null, values);
-                if ( _id > 0 )
-                    returnUri = MovieContract.RatedMovies.buildRatedUri(_id);
-                else
-                    throw new android.database.SQLException("Failed to insert row into " + uri);
-                break;
-            }
-            case REVENUE: {
-                long _id = db.insert(MovieContract.RevenueMovies.TABLE_NAME, null, values);
-                if ( _id > 0 )
-                    returnUri = MovieContract.RevenueMovies.buildRevenueUri(_id);
-                else
-                    throw new android.database.SQLException("Failed to insert row into " + uri);
-                break;
-            }
-            case BOOKMARKS: {
-                long _id = db.insert(MovieContract.Bookmarks.TABLE_NAME, null, values);
-                if ( _id > 0 )
-                    returnUri = MovieContract.Bookmarks.buildBookmarksUri(_id);
+                    returnUri = MovieContract.Movies.buildPopularUri(_id);
                 else
                     throw new android.database.SQLException("Failed to insert row into " + uri);
                 break;
@@ -249,58 +121,20 @@ public class MovieProvider extends ContentProvider {
         // this makes delete all rows return the number of rows deleted
         if ( null == selection ) selection = "1";
         switch (match) {
-            case POPULAR:
+            case MOVIE:
                 rowsDeleted = db.delete(
-                        MovieContract.PopularMovies.TABLE_NAME, selection, selectionArgs);
+                        MovieContract.Movies.TABLE_NAME, selection, selectionArgs);
                 break;
-            case POPULAR_WITH_ID:
-                rowsDeleted = db.delete(MovieContract.PopularMovies.TABLE_NAME,
-                        MovieContract.PopularMovies.MOVIE_ID + " = ?",
+            case MOVIE_WITH_ID:
+                rowsDeleted = db.delete(MovieContract.Movies.TABLE_NAME,
+                        MovieContract.Movies.MOVIE_ID + " = ?",
                         new String[]{String.valueOf(ContentUris.parseId(uri))});
               /*  // reset _ID
                 db.execSQL("DELETE FROM SQLITE_SEQUENCE WHERE NAME = '" +
-                        MovieContract.PopularMovies.TABLE_NAME + "'");
+                        MovieContract.Movies.TABLE_NAME + "'");
 */
                 break;
-            case REVENUE:
-                rowsDeleted = db.delete(
-                        MovieContract.RevenueMovies.TABLE_NAME, selection, selectionArgs);
-                break;
-            case RATED_WITH_ID:
-                rowsDeleted = db.delete(MovieContract.RevenueMovies.TABLE_NAME,
-                        MovieContract.RevenueMovies.MOVIE_ID + " = ?",
-                        new String[]{String.valueOf(ContentUris.parseId(uri))});
-              /*  // reset _ID
-                db.execSQL("DELETE FROM SQLITE_SEQUENCE WHERE NAME = '" +
-                        MovieContract.RevenueMovies.TABLE_NAME + "'");
-*/
-                break;
-            case RATED:
-                rowsDeleted = db.delete(
-                        MovieContract.RatedMovies.TABLE_NAME, selection, selectionArgs);
-                break;
-            case REVENUE_WITH_ID:
-                rowsDeleted = db.delete(MovieContract.RevenueMovies.TABLE_NAME,
-                        MovieContract.RevenueMovies.MOVIE_ID + " = ?",
-                        new String[]{String.valueOf(ContentUris.parseId(uri))});
-              /*  // reset _ID
-                db.execSQL("DELETE FROM SQLITE_SEQUENCE WHERE NAME = '" +
-                        MovieContract.RevenueMovies.TABLE_NAME + "'");
-*/
-                break;
-            case BOOKMARKS:
-                rowsDeleted = db.delete(
-                        MovieContract.Bookmarks.TABLE_NAME, selection, selectionArgs);
-                break;
-            case BOOKMARKS_WITH_ID:
-                rowsDeleted = db.delete(MovieContract.Bookmarks.TABLE_NAME,
-                        MovieContract.Bookmarks.MOVIE_ID + " = ?",
-                        new String[]{String.valueOf(ContentUris.parseId(uri))});
-              /*  // reset _ID
-                db.execSQL("DELETE FROM SQLITE_SEQUENCE WHERE NAME = '" +
-                        MovieContract.Bookmarks.TABLE_NAME + "'");
-*/
-                break;
+
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
@@ -318,47 +152,14 @@ public class MovieProvider extends ContentProvider {
         int rowsUpdated;
 
         switch (match) {
-            case POPULAR:
-                rowsUpdated = db.update(MovieContract.PopularMovies.TABLE_NAME, values, selection,
+            case MOVIE:
+                rowsUpdated = db.update(MovieContract.Movies.TABLE_NAME, values, selection,
                         selectionArgs);
                 break;
-            case POPULAR_WITH_ID: {
-                rowsUpdated = db.update(MovieContract.PopularMovies.TABLE_NAME,
+            case MOVIE_WITH_ID: {
+                rowsUpdated = db.update(MovieContract.Movies.TABLE_NAME,
                         values,
-                        MovieContract.PopularMovies.MOVIE_ID + " = ?",
-                        new String[] {String.valueOf(ContentUris.parseId(uri))});
-                break;
-            }
-            case RATED:
-                rowsUpdated = db.update(MovieContract.RatedMovies.TABLE_NAME, values, selection,
-                        selectionArgs);
-                break;
-            case RATED_WITH_ID: {
-                rowsUpdated = db.update(MovieContract.RatedMovies.TABLE_NAME,
-                        values,
-                        MovieContract.RatedMovies.MOVIE_ID + " = ?",
-                        new String[] {String.valueOf(ContentUris.parseId(uri))});
-                break;
-            }
-            case REVENUE:
-                rowsUpdated = db.update(MovieContract.RevenueMovies.TABLE_NAME, values, selection,
-                        selectionArgs);
-                break;
-            case REVENUE_WITH_ID: {
-                rowsUpdated = db.update(MovieContract.RevenueMovies.TABLE_NAME,
-                        values,
-                        MovieContract.RevenueMovies.MOVIE_ID + " = ?",
-                        new String[] {String.valueOf(ContentUris.parseId(uri))});
-                break;
-            }
-            case BOOKMARKS:
-                rowsUpdated = db.update(MovieContract.Bookmarks.TABLE_NAME, values, selection,
-                        selectionArgs);
-                break;
-            case BOOKMARKS_WITH_ID: {
-                rowsUpdated = db.update(MovieContract.Bookmarks.TABLE_NAME,
-                        values,
-                        MovieContract.Bookmarks.MOVIE_ID + " = ?",
+                        MovieContract.Movies.MOVIE_ID + " = ?",
                         new String[] {String.valueOf(ContentUris.parseId(uri))});
                 break;
             }
@@ -376,13 +177,13 @@ public class MovieProvider extends ContentProvider {
         final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
         final int match = sUriMatcher.match(uri);
         switch (match) {
-            case POPULAR:
+            case MOVIE:
                 db.beginTransaction();
                 int returnCount = 0;
                 try {
                     for (ContentValues value : values) {
 
-                        long _id = db.insert(MovieContract.PopularMovies.TABLE_NAME, null, value);
+                        long _id = db.insert(MovieContract.Movies.TABLE_NAME, null, value);
                         if (_id != -1) {
                             returnCount++;
                         }
@@ -393,58 +194,6 @@ public class MovieProvider extends ContentProvider {
                 }
                 getContext().getContentResolver().notifyChange(uri, null);
                 return returnCount;
-            case RATED:
-                db.beginTransaction();
-                returnCount = 0;
-                try {
-                    for (ContentValues value : values) {
-
-                        long _id = db.insert(MovieContract.RatedMovies.TABLE_NAME, null, value);
-                        if (_id != -1) {
-                            returnCount++;
-                        }
-                    }
-                    db.setTransactionSuccessful();
-                } finally {
-                    db.endTransaction();
-                }
-                getContext().getContentResolver().notifyChange(uri, null);
-                return returnCount;
-            case REVENUE:
-                db.beginTransaction();
-                returnCount = 0;
-                try {
-                    for (ContentValues value : values) {
-
-                        long _id = db.insert(MovieContract.RevenueMovies.TABLE_NAME, null, value);
-                        if (_id != -1) {
-                            returnCount++;
-                        }
-                    }
-                    db.setTransactionSuccessful();
-                } finally {
-                    db.endTransaction();
-                }
-                getContext().getContentResolver().notifyChange(uri, null);
-                return returnCount;
-            case BOOKMARKS:
-                db.beginTransaction();
-                returnCount = 0;
-                try {
-                    for (ContentValues value : values) {
-
-                        long _id = db.insert(MovieContract.PopularMovies.TABLE_NAME, null, value);
-                        if (_id != -1) {
-                            returnCount++;
-                        }
-                    }
-                    db.setTransactionSuccessful();
-                } finally {
-                    db.endTransaction();
-                }
-                getContext().getContentResolver().notifyChange(uri, null);
-                return returnCount;
-
 
             default:
                 return super.bulkInsert(uri, values);
