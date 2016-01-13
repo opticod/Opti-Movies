5f771bd8dd4a8bfc234842212e7cfc1152a3e45a
Anupam Das <anupam.das.bwn@gmail.com>
2016-01-14 00:28:35 +0530

    Utility Added

--- app/src/main/java/work/technie/popularmovies/utils/Utility.java
+++ app/src/main/java/work/technie/popularmovies/utils/Utility.java
@@ -2,6 +2,8 @@ package work.technie.popularmovies.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
 
 import work.technie.popularmovies.R;
@@ -16,4 +18,21 @@ public class Utility {
         return prefs.getString(context.getString(R.string.pref_sort_key),
                 context.getString(R.string.pref_sort_default));
     }
+    public static boolean hasNetworkConnection(Context context) {
+        boolean hasConnectedWifi = false;
+        boolean hasConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    hasConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    hasConnectedMobile = true;
+        }
+        return hasConnectedWifi || hasConnectedMobile;
+    }
+
 }
