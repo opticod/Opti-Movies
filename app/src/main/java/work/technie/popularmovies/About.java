5025eb67ec8c576274bd6e1d59e4fcd32cf3f732
Anupam Das <anupam.das.bwn@gmail.com>
2016-02-13 19:52:09 +0530

    Adding About Section

---
+++ app/src/main/java/work/technie/popularmovies/About.java
@@ -0,0 +1,16 @@
+package work.technie.popularmovies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by anupam on 13/2/16.
+ */
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
