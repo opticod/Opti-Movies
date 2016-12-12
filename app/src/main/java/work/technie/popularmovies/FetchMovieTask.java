c2fe9176d035d79a910edcbcdb7550330723a20d
Anupam Das <anupam.das.bwn@gmail.com>
2016-12-13 05:07:25 +0530

    FEAT: Ability to fetch asynchronously tv shows

--- app/src/main/java/work/technie/popularmovies/FetchMovieTask.java
+++ app/src/main/java/work/technie/popularmovies/FetchMovieTask.java
@@ -32,8 +32,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Vector;
 
+import work.technie.popularmovies.activity.BaseActivity;
 import work.technie.popularmovies.data.MovieContract;
-import work.technie.popularmovies.utils.Utility;
 
 /**
  * Created by anupam on 17/12/15.
@@ -55,7 +55,7 @@ public class FetchMovieTask extends AsyncTask<String, Void, Void> {
      * Fortunately parsing is easy:  constructor takes the JSON string and converts it
      * into an Object hierarchy for us.
      */
-    private void getMovieDataFromJson(String movieJsonStr)
+    private void getMovieDataFromJson(String movieJsonStr, String mode)
             throws JSONException {
 
         // These are the names of the JSON objects that need to be extracted.
@@ -140,7 +140,7 @@ public class FetchMovieTask extends AsyncTask<String, Void, Void> {
                 movieValues.put(MovieContract.Movies.POPULARITY, popularity);
                 movieValues.put(MovieContract.Movies.VOTE_COUNT, vote_count);
                 movieValues.put(MovieContract.Movies.VOTE_AVERAGE, votAvg);
-                movieValues.put(MovieContract.Movies.SORT_BY, Utility.getPreferredSorting(mContext));
+                movieValues.put(MovieContract.Movies.MODE, mode);
                 cVVector.add(movieValues);
             }
             int inserted = 0;
@@ -150,10 +150,108 @@ public class FetchMovieTask extends AsyncTask<String, Void, Void> {
                 cVVector.toArray(cvArray);
                 inserted = mContext.getContentResolver().bulkInsert(MovieContract.Movies.CONTENT_URI, cvArray);
             }
-            //Log.d(LOG_TAG, "FetchPopularMovie Task Complete. " + inserted + " Inserted");
 
         } catch (JSONException e) {
-            //Log.e(LOG_TAG, e.getMessage(), e);
+            e.printStackTrace();
+        }
+    }
+
+    private void getTVDataFromJson(String tvJsonStr, String mode)
+            throws JSONException {
+
+        // These are the names of the JSON objects that need to be extracted.
+        final String TV_ID = "id";
+        final String ORGLANG = "original_language";
+        final String NAME = "name";
+        final String ORGNAME = "original_name";
+        final String OVER = "overview";
+        final String FIRST_AIR_DATE = "first_air_date";
+        final String POSTERPATH = "poster_path";
+        final String POPULARITY = "popularity";
+        final String VOTAVG = "vote_average";
+        final String BACKDROP_PATH = "backdrop_path";
+        final String VOTE_COUNT = "vote_count";
+        final String MODE = "mode";
+        final String PAGE = "page";
+
+
+        final String RESULT = "results";
+        final String POSTER_BASE_URL = "http://image.tmdb.org/t/p/w185";
+        final String BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w500";
+
+        try {
+            JSONObject tvJson = new JSONObject(tvJsonStr);
+            JSONArray tvArray = tvJson.getJSONArray(RESULT);
+
+            String page = tvJson.getString(PAGE);
+
+            Vector<ContentValues> cVVector = new Vector<ContentValues>(tvArray.length());
+
+            for (int i = 0; i < tvArray.length(); i++) {
+
+                String tv_id;
+                String orgLang;
+                String name;
+                String org_name;
+                String overview;
+                String first_air_date;
+                String postURL;
+                String popularity;
+                String votAvg;
+                String vote_count;
+                String backdropURL;
+
+
+                JSONObject movieInfo = tvArray.getJSONObject(i);
+
+                tv_id = movieInfo.getString(TV_ID);
+                orgLang = movieInfo.getString(ORGLANG);
+                name = movieInfo.getString(NAME);
+                org_name = movieInfo.getString(ORGNAME);
+                overview = movieInfo.getString(OVER);
+                first_air_date = movieInfo.getString(FIRST_AIR_DATE);
+                popularity = movieInfo.getString(POPULARITY);
+                votAvg = movieInfo.getString(VOTAVG);
+                backdropURL = movieInfo.getString(BACKDROP_PATH);
+                vote_count = movieInfo.getString(VOTE_COUNT);
+
+
+                postURL = Uri.parse(POSTER_BASE_URL).buildUpon().
+                        appendEncodedPath(movieInfo.getString(POSTERPATH)).build().toString();
+
+                backdropURL = Uri.parse(BACKDROP_BASE_URL).buildUpon().
+                        appendEncodedPath(movieInfo.getString(BACKDROP_PATH)).build().toString();
+
+
+                ContentValues tvValues = new ContentValues();
+
+                // Then add the data, along with the corresponding name of the data type,
+                // so the content provider knows what kind of value is being inserted.
+
+                tvValues.put(MovieContract.TV.PAGE, page);
+                tvValues.put(MovieContract.TV.POSTER_PATH, postURL);
+                tvValues.put(MovieContract.TV.OVERVIEW, overview);
+                tvValues.put(MovieContract.TV.FIRST_AIR_DATE, first_air_date);
+                tvValues.put(MovieContract.TV.TV_ID, tv_id);
+                tvValues.put(MovieContract.TV.ORIGINAL_NAME, org_name);
+                tvValues.put(MovieContract.TV.ORIGINAL_LANGUAGE, orgLang);
+                tvValues.put(MovieContract.TV.NAME, name);
+                tvValues.put(MovieContract.TV.BACKDROP_PATH, backdropURL);
+                tvValues.put(MovieContract.TV.POPULARITY, popularity);
+                tvValues.put(MovieContract.TV.VOTE_COUNT, vote_count);
+                tvValues.put(MovieContract.TV.VOTE_AVERAGE, votAvg);
+                tvValues.put(MovieContract.TV.MODE, mode);
+                cVVector.add(tvValues);
+            }
+            int inserted = 0;
+            // add to database
+            if (cVVector.size() > 0) {
+                ContentValues[] cvArray = new ContentValues[cVVector.size()];
+                cVVector.toArray(cvArray);
+                inserted = mContext.getContentResolver().bulkInsert(MovieContract.TV.CONTENT_URI, cvArray);
+            }
+
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
@@ -172,21 +270,64 @@ public class FetchMovieTask extends AsyncTask<String, Void, Void> {
 
         // Will contain the raw JSON response as a string.
         String movieJsonStr = null;
+        boolean isMovie = true;
 
         try {
             // Construct the URL for the movieAPI query
             // Possible parameters are avaiable at OWM's forecast API page, at
 
-            final String MOVIE_BASE_URL =
-                    "http://api.themoviedb.org/3/discover/movie?";
-            final String SORT_PARAM = "sort_by";
+            String MOVIE_BASE_URL = "https://api.themoviedb.org/3/";
+
+            switch (params[1]) {
+                case BaseActivity.FRAGMENT_TAG_MOV_NOW_PLAYING:
+                    MOVIE_BASE_URL += "movie/now_playing?";
+                    break;
+                case BaseActivity.FRAGMENT_TAG_MOV_POPULAR:
+                    MOVIE_BASE_URL += "movie/popular?";
+                    break;
+
+                case BaseActivity.FRAGMENT_TAG_MOV_TOP_RATED:
+                    MOVIE_BASE_URL += "movie/top_rated?";
+                    break;
+
+                case BaseActivity.FRAGMENT_TAG_MOV_UPCOMING:
+                    MOVIE_BASE_URL += "movie/upcoming?";
+                    break;
+
+                case BaseActivity.FRAGMENT_TAG_TV_AIRING_TODAY:
+                    isMovie = false;
+                    MOVIE_BASE_URL += "/tv/airing_today?";
+                    break;
+
+                case BaseActivity.FRAGMENT_TAG_TV_ON_THE_AIR:
+                    isMovie = false;
+                    MOVIE_BASE_URL += "/tv/on_the_air?";
+                    break;
+
+                case BaseActivity.FRAGMENT_TAG_TV_POPULAR:
+                    isMovie = false;
+                    MOVIE_BASE_URL += "/tv/popular?";
+                    break;
+
+                case BaseActivity.FRAGMENT_TAG_TV_TOP_RATED:
+                    isMovie = false;
+                    MOVIE_BASE_URL += "/tv/top_rated?";
+                    break;
+
+                default:
+                    break;
+            }
+
             final String APPID_PARAM = "api_key";
             final String PAGE_PARAM = "page";
+            final String LANG_PARAM = "language";
+            final String REGION_PARAM = "region";
 
             Uri builtUri = Uri.parse(MOVIE_BASE_URL).buildUpon()
-                    .appendQueryParameter(SORT_PARAM, params[0])
                     .appendQueryParameter(APPID_PARAM, BuildConfig.MOVIE_DB_API_KEY)
-                    .appendQueryParameter(PAGE_PARAM, params[1])
+                    .appendQueryParameter(LANG_PARAM, "hi")
+                    .appendQueryParameter(PAGE_PARAM, "1")
+                    .appendQueryParameter(REGION_PARAM, "IN")
                     .build();
 
             URL url = new URL(builtUri.toString());
@@ -217,7 +358,11 @@ public class FetchMovieTask extends AsyncTask<String, Void, Void> {
                 return null;
             }
             movieJsonStr = buffer.toString();
-            getMovieDataFromJson(movieJsonStr);
+            if (isMovie) {
+                getMovieDataFromJson(movieJsonStr, params[1]);
+            } else {
+                getTVDataFromJson(movieJsonStr, params[1]);
+            }
         } catch (IOException e) {
             //Log.e(LOG_TAG, "Error ", e);
             // If the code didn't successfully get the movie data, there's no point in attemping
