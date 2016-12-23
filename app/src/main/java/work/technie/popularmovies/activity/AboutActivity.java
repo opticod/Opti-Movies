607412eafaa88190b9cb945a832145f5d7b1bfbb
Anupam Das <anupam.das.bwn@gmail.com>
2016-12-23 09:17:41 +0530

    UPDATE: updated about layout + added github icon

--- app/src/main/java/work/technie/popularmovies/activity/AboutActivity.java
+++ app/src/main/java/work/technie/popularmovies/activity/AboutActivity.java
@@ -1,10 +1,11 @@
 package work.technie.popularmovies.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
 
 import work.technie.popularmovies.R;
 
@@ -19,8 +20,13 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        TextView txtView = (TextView) findViewById(R.id.about_text);
-        txtView.setMovementMethod(LinkMovementMethod.getInstance());
+        findViewById(R.id.github_link).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/opticod/Opti-Movies"));
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
