a54b90a4f3e6cd8de59262ace00c2865a5f955c5
Anupam Das <anupam.das.bwn@gmail.com>
2016-12-25 03:39:43 +0530

    PRODUCTION: Updated License

--- app/src/main/java/work/technie/popularmovies/Constants.java
+++ app/src/main/java/work/technie/popularmovies/Constants.java
@@ -1,5 +1,19 @@
 package work.technie.popularmovies;
-
+/*
+ * Copyright (C) 2017 Anupam Das
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import work.technie.popularmovies.data.MovieContract;
 import work.technie.popularmovies.data.MovieContract.TVDetails;
 
